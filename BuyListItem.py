b5ec0cc8b51f0ee66c63e87bf4d48605ebf73b95
FishBowlLab <92322149+FishBowlLab@users.noreply.github.com>
2024-01-25 13:39:48 -0500

    Delete BuyListItem.py
    Renamed as Card.py

--- BuyListItem.py
+++
@@ -1,8 +0,0 @@
-class BuyListItem:
-    def __init__(self) -> None:
-        self.name=None
-        self.set=None
-        self.condition='NM'     # Fix this until I can parse the article tag     
-        self.finish=None        # foil or regular
-        self.price=None
-        
